6eaa1a48384a6d6853fb4f95a459791ef0e8f4db
Purushottam Kumar <purushottam2fx@gmail.com>
2026-02-20 10:27:33 +0530

    add compressImage type in update user profile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const {upload} = require("../middleware/upload");
-const { getAllUsers,updateUser,patchUser ,deleteUser} = require("../controllers/userControllers");
-const {authMiddleware}=require("../middleware/authMiddleware");
+const { upload, compressImage } = require("../middleware/upload");
+const {
+  getAllUsers,
+  updateUser,
+  patchUser,
+  deleteUser,
+} = require("../controllers/userControllers");
+const { authMiddleware } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/", getAllUsers);
-router.put("/update/:id", upload.single("user_pic"), updateUser);
-router.patch("/patch/:id", upload.single("user_pic"), patchUser);
-router.delete("/delete/:id", authMiddleware,deleteUser);
-
-
+router.put("/update/:id", upload.single("user_pic"), compressImage, updateUser);
+router.patch("/patch/:id", upload.single("user_pic"), compressImage, patchUser);
+router.delete("/delete/:id", authMiddleware, deleteUser);
 
 module.exports = router;
